fc741e663ff456e443c8b8232d5689eb132df5e5
Chris <chris.tisserat@gmail.com>
2020-11-18 15:00:24 -0800

    3 sentance functions prepared

--- main.js
+++ main.js
@@ -5,5 +5,20 @@ function numberGenerator (number) {
 };
 
 function sectionOne (randomNumber) {
+//will need to flesh out arrayItems for readbility
+  let sectionOneArray = ['will', 'should', 'need to', 'may', 
+'might'];
+  return sectionOneArray[randomNumber];
+};
 
+function sectionTwo (randomNumber) {
+  //will need to flesh out arrayItems for readbility
+  let sectionTwoArray = ['require', 'should', 'need to', 'may require', 'want to'];
+  return sectionTwoArray[randomNumber];
+}
+
+function sectionThree (randomNumber) {
+    //will need to flesh out arrayItems for readbility
+  let sectionThreeArray = ['require', 'should', 'need to', 'may require', 'want to'];
+  return sectionThreeArray[randomNumber];
 }
